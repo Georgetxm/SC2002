05c9b39e8df79c7dcd248061342fc15ff0c77c15
Georgetxm <xuanmingt@gmail.com>
2023-11-09 13:40:04 +0800

    Update: Camp controller method params and return type.

--- controllers/CampController.java
+++ controllers/CampController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map.Entry;
 
 import core.CampInfo;
@@ -9,19 +10,34 @@ import types.Role;
 
 public interface CampController {
 	public int addCamp(CampInfo info);
-	public String addUser(int campid, int userid, Role role);
-	public int addEnquiry(int campid, String enquiry);
-	public int addSuggestion(int campid, int suggestionid);
-	public boolean deleteCamp(int campid);
-	public boolean deleteUser(int campid, int userid);
-	public boolean deleteEnquiry(int campid, int enquiryid);
-	public boolean deleteSuggestion(int campid, int suggestionid);
-	public HashMap<String,Integer> getCamps(Entry<CampAspects,? extends Object> filter);
-	public CampInfo getCampDetails(int campid);
-	public String getCampStudentList(int campid);
-	public Entry<Integer,Role>[] getCampParticipantID(int campid);
+
+	public boolean addUser(int campId, String userId, Role role);
+
+	public boolean addEnquiry(int campId, String creatorId, int enquiryId);
+
+	public boolean addSuggestion(int campId, String creatorId, int suggestionId);
+
+	public boolean deleteCamp(int campId);
+
+	public boolean removeAttendeeFromCamp(int campId, String userId);
+
+	public boolean deleteEnquiry(int campId, String creatorId, int enquiryid);
+
+	public boolean deleteSuggestion(int campId, String creatorId, int suggestionid);
+
+	public HashMap<String, Integer> getCamps(Entry<CampAspects, ? extends Object> filter);
+
+	public CampInfo getCampDetails(int campId);
+
+	public HashSet<String> getCampAttendees(int campId);
+
+	public Entry<Integer, Role>[] getCampParticipantID(int campId);
+
 	public int getSuggestionOwner(int campid, int suggestionid);
+
 	public boolean editCampDetails(int campid, Entry<CampAspects, ? extends Object> detail);
+
 	public boolean isAttendeeFull(int campid);
+
 	public boolean isCommiteeFull(int campid);
 }

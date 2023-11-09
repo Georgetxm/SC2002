8b39206888f6ba0ee740fd9cd3c65c4ef4a78873
JaredXwos <hatoandaburedo@gmail.com>
2023-11-09 23:33:05 +0800

    Linked query view all camp menu

--- cams/CamsInteraction.java
+++ cams/CamsInteraction.java
@@ -9,6 +9,7 @@ import camsAction.doDeleteSuggestion;
 import camsAction.doEditSuggestion;
 import camsAction.doNothing;
 import camsAction.doSubmitCamp;
+import camsAction.queryViewAllCampsMenu;
 import interactions.MenuChoice;
 import types.Perms;
 	
@@ -16,7 +17,7 @@ import types.Perms;
 public class CamsInteraction{
 	//Menu choices
 	private static MenuChoice 
-	viewAllCamps 		= new MenuChoice(Perms.DEFAULT, 					"View All Camps", 			new doNothing()),
+	viewAllCamps 		= new MenuChoice(Perms.DEFAULT, 					"View All Camps", 			new queryViewAllCampsMenu()),
 	approveSuggestion	= new MenuChoice(Perms.APPROVE_CAMP_SUGGESTION, 	"Approve Suggestion", 		new doApproveSuggestion()),
 	editSuggestion		= new MenuChoice(Perms.EDIT_CAMP_SUGGESTION,		"Edit this suggestion",		new doEditSuggestion()),
 	deleteSuggestion	= new MenuChoice(Perms.DELETE_CAMP_SUGGESTION,		"Delete this suggestion",	new doDeleteSuggestion()),

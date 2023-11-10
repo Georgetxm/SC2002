e666f87c8925200c7865558e499d2f6c12c40f9d
JaredXwos <hatoandaburedo@gmail.com>
2023-11-10 12:27:54 +0800

    Added stubs in preparation for SingleCampMenu (static menu)

--- cams/CamsInteraction.java
+++ cams/CamsInteraction.java
@@ -10,8 +10,12 @@ import camsAction.doDeleteSuggestion;
 import camsAction.doEditSuggestion;
 import camsAction.doSubmitCamp;
 import camsAction.doSubmitEdit;
+import camsAction.doSubmitEnquiry;
+import camsAction.doSubmitSuggestion;
 import camsAction.queryFilterCampByMenu;
 import camsAction.queryViewAllCampsMenu;
+import camsAction.queryViewAllEnquiryMenu;
+import camsAction.queryViewOwnEnquiryMenu;
 import interactions.MenuChoice;
 import types.Perms;
 	
@@ -27,7 +31,11 @@ public class CamsInteraction{
 	filterCampBy		= new MenuChoice(Perms.DEFAULT, 					"Add Filter",				new queryFilterCampByMenu()),
 	removeCampFilter	= new MenuChoice(Perms.DEFAULT, 					"Remove Filter",			new queryViewAllCampsMenu()),
 	deleteCamp			= new MenuChoice(Perms.DELETE_CAMP,					"Delete this camp",			new doDeleteCamp()),
-	editCamp			= new MenuChoice(Perms.EDIT_CAMP,					"Edit this camp details",	new doSubmitEdit());
+	editCamp			= new MenuChoice(Perms.EDIT_CAMP,					"Edit this camp details",	new doSubmitEdit()),
+	viewOwnEnquiry		= new MenuChoice(Perms.SUBMIT_CAMP_ENQUIRY,			"View your own enquiries",	new queryViewOwnEnquiryMenu()),
+	viewAllEnquiry		= new MenuChoice(Perms.VIEW_CAMP_ENQUIRY,			"View all enquiries",		new queryViewAllEnquiryMenu()),
+	submitEnquiry		= new MenuChoice(Perms.SUBMIT_CAMP_ENQUIRY,			"Submit a new enquiry",		new doSubmitEnquiry()),
+	submitSuggestion	= new MenuChoice(Perms.SUBMIT_CAMP_SUGGESTION,		"Submit a new suggestion",	new doSubmitSuggestion());
 	private static List<MenuChoice>
 	singleSuggestionChoice = Arrays.asList(
 		approveSuggestion,

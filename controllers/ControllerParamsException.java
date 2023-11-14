75384c26a08956c89829dbfcdd9c771e242a0f32
Georgetxm <xuanmingt@gmail.com>
2023-11-15 00:12:01 +0800

    Add: Custom exception class

---
+++ controllers/ControllerParamsException.java
@@ -0,0 +1,13 @@
+package controllers;
+
+public class ControllerParamsException extends Exception {
+	public ControllerParamsException(String info) {
+		super(info);
+	}
+
+	/**
+	 * Automatically generated serial number for serialisation
+	 */
+	private static final long serialVersionUID = 3116206905553311529L;
+
+}

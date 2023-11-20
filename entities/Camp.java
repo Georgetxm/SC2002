d049636180a75f4b8a01ef51e0e21e6f02a7b5a8
Georgetxm <xuanmingt@gmail.com>
2023-11-20 17:42:12 +0800

    Javadocs for Camp class

--- entities/Camp.java
+++ entities/Camp.java
@@ -5,6 +5,23 @@ import java.util.HashSet;
 
 import types.CampAspect;
 
+/**
+ * Represents a Camp object
+ * 
+ * 
+ * A Camp contains the CampInfo attribute which contains the information of the
+ * camp @see CampInfo
+ * with each information being an Aspect of the camp @see CampAspect
+ * A camp object also contains userIds of attendees and camp committee, as well
+ * as the ids of enquiries and suggestions tied to this camp object
+ * Each camp object has a unique campId which is an auto-incremented static
+ * attribute
+ * 
+ * @author Teo Xuan Ming
+ * @version 1.0
+ * @since 2021-11-12
+ */
+
 public class Camp {
     private CampInfo campInfo;
     private final int campid;
@@ -16,6 +33,16 @@ public class Camp {
     private HashSet<Integer> enquiries = new HashSet<Integer>();
     private HashSet<Integer> suggestions = new HashSet<Integer>();
 
+    /**
+     * Constructor for Camp. Intended use is for creating a new camp when entering
+     * information from the CLI
+     * 
+     * @param campInfo
+     * @param attendees
+     * @param campCommittee
+     * @param visibility
+     * @param creationDate
+     */
     public Camp(CampInfo campInfo, HashSet<String> attendees, HashSet<String> campCommittee,
             boolean visibility, LocalDate creationDate) {
         this.campInfo = campInfo;
@@ -26,6 +53,19 @@ public class Camp {
         this.creationDate = creationDate;
     }
 
+    /**
+     * Constructor for Camp. Intended use is for creating a new camp when reading
+     * from
+     * the camp_list.csv where enquiries and suggestions ids are already included
+     * 
+     * @param campInfo
+     * @param attendees
+     * @param campCommittee
+     * @param visibility
+     * @param creationDate
+     * @param enquiries
+     * @param suggestions
+     */
     public Camp(CampInfo campInfo, HashSet<String> attendees, HashSet<String> campCommittee,
             boolean visibility, LocalDate creationDate, HashSet<Integer> enquiries, HashSet<Integer> suggestions) {
         this.campInfo = campInfo;
@@ -36,39 +76,87 @@ public class Camp {
         this.creationDate = creationDate;
     }
 
+    /**
+     * Getter for campId
+     * 
+     * @return the campId
+     */
     public int getCampid() {
         return this.campid;
     }
 
+    /**
+     * Getter for campInfo object
+     * 
+     * @return the campInfo object
+     */
     public CampInfo getCampInfo() {
         return this.campInfo;
     }
 
+    /**
+     * Getter for visibility
+     * 
+     * @return the visibility
+     */
     public boolean getVisibility() {
         return this.visibility;
     }
 
+    /**
+     * Setter for visibility
+     * 
+     * @param visibility
+     * @return true if visibility is successfully set, false otherwise
+     */
     public boolean setVisibility(boolean visibility) {
         this.visibility = visibility;
         return true;
     }
 
+    /**
+     * Getter for creationDate
+     * 
+     * @return the creationDate
+     */
     public LocalDate getCreationDate() {
         return this.creationDate;
     }
 
+    /**
+     * Getter for attendees
+     * 
+     * @return the attendees
+     */
     public HashSet<String> getAttendees() {
         return this.attendees;
     }
 
+    /**
+     * Returns true if the userId is an attendee of this camp
+     * 
+     * @param userId
+     * @return true if the userId is an attendee of this camp
+     */
     public boolean isCampAttendee(String userId) {
         return this.attendees.contains(userId);
     }
 
+    /**
+     * Returns true if the camp is full
+     * 
+     * @return true if the camp is full
+     */
     public boolean isCampAttendeeFull() {
         return this.attendees.size() >= (int) this.campInfo.info().get(CampAspect.SLOTS);
     }
 
+    /**
+     * Adds a userId to the attendees HashSet
+     * 
+     * @param userId
+     * @return true if the userId is successfully added, false otherwise
+     */
     public boolean addAttendee(String userId) {
         if (this.isCampAttendeeFull()) {
             return false;
@@ -80,6 +168,12 @@ public class Camp {
         return true;
     }
 
+    /**
+     * Removes a userId from the attendees HashSet
+     * 
+     * @param userId
+     * @return true if the userId is successfully removed, false otherwise
+     */
     public boolean removeAttendee(String userId) {
         if (!this.isCampAttendee(userId)) {
             return false;
@@ -90,18 +184,40 @@ public class Camp {
         return true;
     }
 
+    /**
+     * Getter for campCommittee
+     * 
+     * @return campCommittee HashSet
+     */
     public HashSet<String> getCampCommittee() {
         return this.campCommittee;
     }
 
+    /**
+     * Returns true if the userId is a camp committee of this camp
+     * 
+     * @param userId
+     * @return true if the userId is a camp committee of this camp
+     */
     public boolean isCampCommittee(String userId) {
         return this.campCommittee.contains(userId);
     }
 
+    /**
+     * Returns true if the camp committee is full
+     * 
+     * @return true if the camp committee is full
+     */
     public boolean isCampCommitteeFull() {
         return this.campCommittee.size() >= (int) this.campInfo.info().get(CampAspect.COMMITTEESLOTS);
     }
 
+    /**
+     * Adds a userId to the campCommittee HashSet
+     * 
+     * @param userId
+     * @return true if the userId is successfully added, false otherwise
+     */
     public boolean addCommittee(String userId) {
         if (this.isCampCommitteeFull()) {
             System.out.println("Camp committee is full");
@@ -123,6 +239,12 @@ public class Camp {
         return true;
     }
 
+    /**
+     * Removes a userId from the campCommittee HashSet
+     * 
+     * @param userId
+     * @return true if the userId is successfully removed, false otherwise
+     */
     public boolean removeCommittee(String userId) {
         if (!this.isCampCommittee(userId)) {
             return false;
@@ -133,29 +255,63 @@ public class Camp {
         return true;
     }
 
+    /**
+     * Getter for enquiries
+     * 
+     * @return enquiries HashSet
+     */
     public HashSet<Integer> getEnquiries() {
         return this.enquiries;
     }
 
+    /**
+     * Adds an enquiryId to the enquiries HashSet
+     * 
+     * @param enquiryId
+     * @return
+     */
     public boolean addEnquiry(int enquiryId) {
         this.enquiries.add(enquiryId);
         return true;
     }
 
+    /**
+     * Removes an enquiryId from the enquiries HashSet
+     * 
+     * @param enquiryId
+     * @return true if the enquiryId is successfully removed, false otherwise
+     */
     public boolean removeEnquiry(int enquiryId) {
         this.enquiries.remove(enquiryId);
         return true;
     }
 
+    /**
+     * Getter for suggestions
+     * 
+     * @return suggestions HashSet
+     */
     public HashSet<Integer> getSuggestions() {
         return this.suggestions;
     }
 
+    /**
+     * Adds a suggestionId to the suggestions HashSet
+     * 
+     * @param suggestionId
+     * @return true if the suggestionId is successfully added, false otherwise
+     */
     public boolean addSuggestion(int suggestionId) {
         this.suggestions.add(suggestionId);
         return true;
     }
 
+    /**
+     * Removes a suggestionId from the suggestions HashSet
+     * 
+     * @param suggestionId
+     * @return true if the suggestionId is successfully removed, false otherwise
+     */
     public boolean removeSuggestion(int suggestionId) {
         this.suggestions.remove(suggestionId);
         return true;

94cfdc1c537bb98bab53720f681e4d4bc4871305
JaredXwos <hatoandaburedo@gmail.com>
2023-11-25 18:19:00 +0800

    Added invalid userid/password message to login

--- cams/Login.java
+++ cams/Login.java
@@ -5,14 +5,23 @@ import java.util.Scanner;
 
 import entities.User;
 
+/**
+ * Class that deals with logging in
+ */
 public final class Login {
+	/**
+	 * Takes in a bunch of users and logs the user in
+	 * @param s universal scanner
+	 * @param userlist list of users extracted from the file
+	 * @return userid userid of current user
+	 */
 	public static final String getCurrentUser(Scanner s,HashMap<String,User> userlist) {
 		while(true) {
 			System.out.println("User ID:");
 			String userid = s.nextLine();
 			System.out.println("Password:");
 			if(userlist.containsKey(userid)&&userlist.get(userid).getPassword().equals(s.nextLine())) return userid;
-			System.out.println(userlist.get(userid).getPassword());
+			else System.out.println("Invalid password or id");
 		}
 	}
 }

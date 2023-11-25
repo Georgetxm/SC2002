b244c8e5ac1afbf38051d2227d1417e4cc352991
JaredXwos <hatoandaburedo@gmail.com>
2023-11-26 05:10:37 +0800

    Change password

--- camsAction/doChangePassword.java
+++ camsAction/doChangePassword.java
@@ -13,7 +13,7 @@ public class doChangePassword extends Interaction {
 	public Interaction run(String currentuser, Scanner s, Controller control)
 			throws UserInfoMissingException, MissingRequestedDataException {
 		System.out.println("Type your new password");
-		control.User().changePassword(s.nextLine());
+		control.User().changePassword(s.nextLine(),currentuser);
 		System.out.println("Password changed");
 		return CamsInteraction.startmenu(currentuser);
 	}

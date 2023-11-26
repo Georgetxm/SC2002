820398e8d7ad01b6151fd9439baf72887523de48
Georgetxm <xuanmingt@gmail.com>
2023-11-26 19:48:31 +0800

    Javadocs

--- types/Role.java
+++ types/Role.java
@@ -1,7 +1,25 @@
 package types;
 
+/**
+ * Represents the Role ENUM
+ * Used to represent the role of a user in a camp
+ * 
+ * @author Teo Xuan Ming
+ * @version 1.1
+ * @since 2021-11-24
+ */
+
 public enum Role {
+	/**
+	 * The role of a student who is attending the camp
+	 */
 	ATTENDEE,
+	/**
+	 * The role of a student who is a committee member in the camp
+	 */
 	COMMITTEE,
+	/**
+	 * The role of a staff who is in charge of the camp
+	 */
 	STAFF
 }
